d45cac03f121fedd06ea58dd04f48ec056549841
William <wprich704@gmail.com>
2021-10-03 07:31:00 -0700

    Added pop up markers

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -10,8 +10,15 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
+  //  Add a marker to the map for Los Angeles, California.
+  L.circle([34.0522, -118.2437], {
+    radius: 300,
+    fillColor: '#ffffa1',
+    color: 'black'
+ }).addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox://styles/mapbox/dark-v10',
