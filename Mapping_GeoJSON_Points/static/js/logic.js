0610c8dace5771432369635c3aa9419637c66330
William <wprich704@gmail.com>
2021-10-03 12:56:04 -0700

    GeoJSON Data

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,37 +1,54 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      35.1495, -90.0489
-    ],
-    zoom: 5
-  });
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Coordinates for each point to be used in the line.
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [30.1974, -97.6663],
-  [43.6775, -79.6308],
-  [40.6417, -73.7809],
-  [25.7616, -80.1917]
-];
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-  color: "blue",
-  weight: 4,
-  opacity: 0.5,
-  dashArray: "20 20"
+// Grabbing our GeoJSON data.
+//L.geoJSON(sanFranAirport).addTo(map);
+
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer){
+      console.log(layer);
+      layer.bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h2> <hr> <h3> Airport Name: " + feature.properties.name + "</h3>");
+    }
 }).addTo(map);
 
+// // Create a polyline using the line coordinates and make the line yellow.
+// L.polyline(line, {
+//   color: "blue",
+//   weight: 4,
+//   opacity: 0.5,
+//   dashArray: "20 20"
+// }).addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox://styles/mapbox/light-v10',
+    id: 'mapbox://styles/mapbox/outdoors-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
